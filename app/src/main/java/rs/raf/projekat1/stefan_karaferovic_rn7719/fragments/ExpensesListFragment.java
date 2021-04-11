95e6cf10e82f492b1bfe139e36e2b447209d92ac
stefank-29 <stefankaraferovic@gmail.com>
2021-04-11 14:26:28 +0200

    lists fragment tabs

---
+++ app/src/main/java/rs/raf/projekat1/stefan_karaferovic_rn7719/fragments/ExpensesListFragment.java
@@ -0,0 +1,7 @@
+package rs.raf.projekat1.stefan_karaferovic_rn7719.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class ExpensesListFragment extends Fragment {
+
+}
---
+++ app/src/main/java/rs/raf/projekat1/stefan_karaferovic_rn7719/fragments/IncomesListFragment.java
@@ -0,0 +1,6 @@
+package rs.raf.projekat1.stefan_karaferovic_rn7719.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class IncomesListFragment extends Fragment {
+}
