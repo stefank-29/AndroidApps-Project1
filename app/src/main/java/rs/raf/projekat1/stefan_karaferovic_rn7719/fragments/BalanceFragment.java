9ff2dd733bd689bc1101495e5c889ac6ef504142
stefank-29 <stefankaraferovic@gmail.com>
2021-04-11 13:07:37 +0200

    balance fragment done

--- app/src/main/java/rs/raf/projekat1/stefan_karaferovic_rn7719/fragments/BalanceFragment.java
+++ app/src/main/java/rs/raf/projekat1/stefan_karaferovic_rn7719/fragments/BalanceFragment.java
@@ -1,11 +1,78 @@
 package rs.raf.projekat1.stefan_karaferovic_rn7719.fragments;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import rs.raf.projekat1.stefan_karaferovic_rn7719.R;
+import rs.raf.projekat1.stefan_karaferovic_rn7719.viewmodels.BalanceViewModel;
 
 public class BalanceFragment extends Fragment {
+
+    // View comps
+    private TextView incomesSum;
+    private TextView expensesSum;
+    private TextView balanceDifference;
+
+    // View model
+    BalanceViewModel balanceViewModel;
+
     public BalanceFragment() {
         super(R.layout.fragment_balance);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        balanceViewModel = new ViewModelProvider(this).get(BalanceViewModel.class);
+        init(view);
+    }
+
+    private void init(View view) {
+        initView(view);
+        initObservers(view);
+
+    }
+
+    private void initView(View view) {
+        incomesSum = view.findViewById(R.id.balanceIncomeSum);
+        expensesSum = view.findViewById(R.id.balanceExpensesSum);
+        balanceDifference = view.findViewById(R.id.balanceDifference);
+
+    }
+
+    private void initObservers(View view) {
+        balanceViewModel.getIncomes().observe(getViewLifecycleOwner(), incomes -> {
+            int difference = balanceViewModel.getDifference();
+
+            incomesSum.setText(String.valueOf(balanceViewModel.getIncomesSum()));
+            balanceDifference.setText(String.valueOf(difference));
+
+            if (difference >= 0) {
+                balanceDifference.setTextColor(getResources().getColor(R.color.green));
+            } else {
+                balanceDifference.setTextColor(getResources().getColor(R.color.secondaryDarkColor));
+            }
+        });
+
+        balanceViewModel.getExpenses().observe(getViewLifecycleOwner(), expenses -> {
+            int difference = balanceViewModel.getDifference();
+
+            expensesSum.setText(String.valueOf(balanceViewModel.getExpensesSum()));
+            balanceDifference.setText(String.valueOf(balanceViewModel.getDifference()));
+
+            if (difference >= 0) {
+                balanceDifference.setTextColor(getResources().getColor(R.color.green));
+            } else {
+                balanceDifference.setTextColor(getResources().getColor(R.color.secondaryDarkColor));
+            }
+        });
+
+
     }
 }
--- app/src/main/java/rs/raf/projekat1/stefan_karaferovic_rn7719/viewmodels/BalanceViewModel.java
+++ app/src/main/java/rs/raf/projekat1/stefan_karaferovic_rn7719/viewmodels/BalanceViewModel.java
@@ -10,11 +10,31 @@ import java.util.List;
 import rs.raf.projekat1.stefan_karaferovic_rn7719.models.Finance;
 
 public class BalanceViewModel extends ViewModel {
+    public static int counter = 21;
+
     private final MutableLiveData<List<Finance>> incomes = new MutableLiveData<>();
     private final ArrayList<Finance> incomesList = new ArrayList<>();
 
     private final MutableLiveData<List<Finance>> expenses = new MutableLiveData<>();
     private final ArrayList<Finance> expensesList = new ArrayList<>();
+
+    public BalanceViewModel() {
+        // incomes
+        for (int i = 1; i <= 10; i++) {
+            Finance finance = new Finance(i, "Uplata", 150, "Opis neki");
+            incomesList.add(finance);
+        }
+        ArrayList<Finance> listToSubmit = new ArrayList<>(incomesList);
+        incomes.setValue(listToSubmit);
+
+        // expenses
+        for (int i = 11; i <= 20; i++) {
+            Finance finance = new Finance(i, "Isplata", 50, "Opis neki");
+            expensesList.add(finance);
+        }
+        ArrayList<Finance> listToSubmit2 = new ArrayList<>(expensesList);
+        expenses.setValue(listToSubmit2);
+    }
 
     // da ne moze da se menja
     public LiveData<List<Finance>> getIncomes() {
@@ -39,5 +59,9 @@ public class BalanceViewModel extends ViewModel {
             sum += expense.getAmount();
         }
         return sum;
+    }
+
+    public int getDifference() {
+        return getIncomesSum() - getExpensesSum();
     }
 }
